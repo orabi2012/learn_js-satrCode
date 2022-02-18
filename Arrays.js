df86810c19d24ad859365f2d606517720703f620
Ahmed Orabi <aorabi@outlook.com>
2022-02-18 13:00:22 +0300

    -

--- Arrays.js
+++ Arrays.js
@@ -1,5 +1,32 @@
 let colors = ["red", "green", "blue"];
 
 console.log(colors);
-console.log(colors[1]);
-console.log(colors[10]);
+console.log(colors[0]);
+console.log(colors.includes('black'));
+colors[0] = 'black' ;
+console.log(colors);
+console.log(colors[0]);
+console.log(colors.includes('black'));
+
+console.log(colors.length)
+
+colors.push('pink');
+
+console.log(colors);
+console.log(colors.length);
+
+let lastcolor = colors.pop();
+
+console.log(colors);
+console.log(lastcolor) ;
+
+colors.unshift(lastcolor);
+console.log(colors);
+
+let pink = colors.shift();
+console.log(pink);
+console.log(colors);
+
+
+
+
