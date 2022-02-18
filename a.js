8ed4a1ea6570ceb6c328c32c7a92cbc7ecb5acb7
Ahmed Orabi <aorabi@outlook.com>
2022-02-18 12:31:54 +0300

    initial

---
+++ a.js
@@ -0,0 +1,5 @@
+let colors = ["red", "green", "blue"];
+
+console.log(colors);
+console.log(colors[1]);
+console.log(colors[10]);
---
+++ main.js
@@ -0,0 +1,31 @@
+
+let MyName="Ahmed Orabi" ,age=38 ;
+const nationality = "Egypt"
+
+MyName = "Ahmed Orabi Sayed"
+
+
+// console.log("welcome To Js Mr :  " + MyName + " my Age = " + age);
+// console.log(nationality)
+
+let x = `ahmed
+orabi
+sayed
+` ;
+// let result = 320 % 160;
+// result = result % 100
+// console.log(result);
+
+// let result = 1 === "1";
+// console.log(result);
+/*let number = 2;
+let result = ++number + 4; // postfix
+console.log(result);
+console.log(number); */
+
+
+let firstNumber = 9 ;
+   let secondNumber = 31 ;
+   let resultTwo = (secondNumber - 1) * firstNumber ;
+
+   console.log( resultTwo);
