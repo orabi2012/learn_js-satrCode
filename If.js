4f6bb98526405004e4dd1905c2722919073cfc2c
Ahmed Orabi <aorabi@outlook.com>
2022-02-18 16:11:29 +0300

    --

---
+++ If.js
@@ -0,0 +1,40 @@
+// let age = 35;
+
+// if (age>35){
+
+//     console.log('you are too old ');
+
+// }else
+// {
+
+//     console.log('you are too young ');
+
+// }
+//if statement
+
+let number = -10;
+
+if (number > 0) {
+console.log('+');
+}else if (number < 0) {
+console.log('-');
+}else {
+console.log('Zero');
+}
+
+//case statement
+switch (true){
+
+case  (number == 0): console.log('zero') ;
+break;
+
+case (number > 0) : console.log('+ve');
+break;
+
+case (number < 0) : console.log('-ve');
+break;
+
+default :console.log('zero') ;
+}
+
+console.log('1'===1);
