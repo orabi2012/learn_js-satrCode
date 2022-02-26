b24a7952fca0c4a6c9e9c5115a3ecc72ca7408b6
Ahmed Orabi <aorabi@outlook.com>
2022-02-26 23:37:26 +0300

    call back functions

--- callback.js
+++ callback.js
@@ -1,27 +1,31 @@
 function sub (n1 , n2) {
 
-    return n1-n2
+    return  'substraction = ' + (n1-n2)
 }
 
 function add (n1 , n2) {
 
-    return n1+n2
+    return 'addition = ' +   (n1+n2)
 }
 function mult (n1 , n2) {
 
-    return n1*n2
+    return 'Multiplication  = ' +  n1*n2
 }
 
 function op (n1,n2, opreation){
 
     return opreation(n1,n2)
+    console.log('------------')
 }
 
-let n1 = 10;
-let n2 = 10;
+let n1 = 50;
+let n2 = 100;
 
 console.log(op(n1,n2 ,add))
 console.log(op(n1,n2 ,sub))
 console.log(op(n1,n2 ,mult))
-console.log(op(n1*2 ,n2*2 ,mult))
+
+
+
+//console.log(op(n1,n2,(f1,f2)=> f1/f2 ))
 
