d7d80b335074dec85b3625c5ab5010deb2fab4c7
Ahmed Orabi <aorabi@outlook.com>
2022-03-04 11:54:13 +0300

    FWD

--- T.js
+++ T.js
@@ -7,7 +7,7 @@ function RT (OT){
 for(var i = 0 ;i <= OT.length -1 ; i++){
 
    // RT += OT[i]+ "  ASCII = " + OT[i].charCodeAt(0) + "\n";
-    RT += OT[i].charCodeAt(0)+"_";
+    RT += OT[i].charCodeAt(0)+"|";
 }
 return RT;
 }
